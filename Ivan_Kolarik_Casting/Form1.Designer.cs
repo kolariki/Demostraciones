8cdb3d2b5f6af8638b097cde1ba5e54f84e4b16d
kolariki <ivankolariki1990@gmail.com>
2022-10-19 23:43:59 -0300

    Casting o Conversión en C#

---
+++ Ivan_Kolarik_Casting/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ivan_Kolarik_Casting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImplicito = new System.Windows.Forms.Button();
+            this.btnConversion = new System.Windows.Forms.Button();
+            this.btnExplicito = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImplicito
+            // 
+            this.btnImplicito.Location = new System.Drawing.Point(353, 120);
+            this.btnImplicito.Name = "btnImplicito";
+            this.btnImplicito.Size = new System.Drawing.Size(75, 23);
+            this.btnImplicito.TabIndex = 0;
+            this.btnImplicito.Text = "Implicito";
+            this.btnImplicito.UseVisualStyleBackColor = true;
+            this.btnImplicito.Click += new System.EventHandler(this.btnImplicito_Click);
+            // 
+            // btnConversion
+            // 
+            this.btnConversion.Location = new System.Drawing.Point(353, 201);
+            this.btnConversion.Name = "btnConversion";
+            this.btnConversion.Size = new System.Drawing.Size(75, 23);
+            this.btnConversion.TabIndex = 1;
+            this.btnConversion.Text = "Conversion";
+            this.btnConversion.UseVisualStyleBackColor = true;
+            this.btnConversion.Click += new System.EventHandler(this.btnConversion_Click);
+            // 
+            // btnExplicito
+            // 
+            this.btnExplicito.Location = new System.Drawing.Point(353, 163);
+            this.btnExplicito.Name = "btnExplicito";
+            this.btnExplicito.Size = new System.Drawing.Size(75, 23);
+            this.btnExplicito.TabIndex = 2;
+            this.btnExplicito.Text = "Explicito";
+            this.btnExplicito.UseVisualStyleBackColor = true;
+            this.btnExplicito.Click += new System.EventHandler(this.btnExplicito_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExplicito);
+            this.Controls.Add(this.btnConversion);
+            this.Controls.Add(this.btnImplicito);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImplicito;
+        private System.Windows.Forms.Button btnConversion;
+        private System.Windows.Forms.Button btnExplicito;
+    }
+}
+
---
+++ Ivan_Kolarik_Casting/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ivan_Kolarik_Casting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImplicito_Click(object sender, EventArgs e)
+        {
+            ConversionImplicita();
+        }
+
+        private void btnExplicito_Click(object sender, EventArgs e)
+        {
+            ConversionExplicita();
+        }
+
+        private void btnConversion_Click(object sender, EventArgs e)
+        {
+            MetodosDeConversion();
+        }
+
+        #region mis métodos
+        private void ConversionImplicita()
+        {
+
+            //CONVERSION IMPLICITA
+            // convierte en forma implicita el valor de randonNumber en la parte derecha a un
+            // tipo int antes de asignarlo a numeroAleatorio            
+            var randomNumber = new Random().Next(0, 100);
+            int numeroAleatorio = randomNumber;
+
+            MessageBox.Show("Su numero de la suerte es: " + numeroAleatorio);
+        }
+
+        private void ConversionExplicita()
+        {
+            //CONVERSION EXPLICITA
+            //se especificxa que el tipo int que esta entre parentesis se va a convertir a la variable myDouble que es de tipo double
+            double myDouble = 7;
+            int myInt = (int)myDouble;
+
+            MessageBox.Show("Mi numero favorito es: " + myInt);
+        }
+
+        private void MetodosDeConversion()
+        {
+            //METODO DE CONVERSION
+            //se convierte un string a un int que luego se suma a otro int
+            string num = "16";
+            int numero1 = Int16.Parse(num);
+            int numero2 = 10;
+
+            MessageBox.Show("La suma de: " + (numero1 + numero2));
+        }
+
+
+    }
+    #endregion
+}
