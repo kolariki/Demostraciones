420c0c9061af63f043ff4d49dac411e7e9f634d1
kolariki <ivankolariki1990@gmail.com>
2022-10-20 14:42:25 -0300

    formulario2

---
+++ WindowsPracticas_IVANKOLARIK/Form2.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsPracticas_IVANKOLARIK
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEdad = new System.Windows.Forms.Button();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEdad
+            // 
+            this.btnEdad.Location = new System.Drawing.Point(353, 212);
+            this.btnEdad.Name = "btnEdad";
+            this.btnEdad.Size = new System.Drawing.Size(100, 23);
+            this.btnEdad.TabIndex = 0;
+            this.btnEdad.Text = "Verificar Edad";
+            this.btnEdad.UseVisualStyleBackColor = true;
+            this.btnEdad.Click += new System.EventHandler(this.btnEdad_Click);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(295, 157);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(33, 15);
+            this.lblEdad.TabIndex = 1;
+            this.lblEdad.Text = "Edad";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(353, 157);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 23);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.btnEdad);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnEdad;
+        private Label lblEdad;
+        private TextBox txtEdad;
+    }
+}
---
+++ WindowsPracticas_IVANKOLARIK/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsPracticas_IVANKOLARIK
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEdad_Click(object sender, EventArgs e)
+        {
+            decimal edad = Convert.ToDecimal(txtEdad.Text);
+
+
+            if ( edad >= 60)
+            {
+                MessageBox.Show("Usted es un persona adulto mayor.");
+            }
+            else if(edad >= 18 && edad < 60 )
+            {
+                MessageBox.Show("Usted es mayor de edad.");
+            }
+            else if (edad < 18 && edad > 0)
+            {
+                MessageBox.Show("Usted es menor de edad.");
+            }
+            else if(edad == 0)
+            {
+                MessageBox.Show("ERROR debe ingresar una edad valida.");
+            }
+
+        }
+
+        }
+    }
+
