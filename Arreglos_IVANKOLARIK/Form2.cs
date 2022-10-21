f70d9401e6e2359068306a2d437f6ae7ef398228
kolariki <ivankolariki1990@gmail.com>
2022-10-21 13:30:16 -0300

    formulario2

---
+++ Arreglos_IVANKOLARIK/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Arreglos_IVANKOLARIK
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void bntPrecios_Click(object sender, EventArgs e)
+        {
+            int[] precios;
+            precios = new int[8];
+            double acumulado = 0;
+
+            for (int i = 0; i < precios.Length; i++)
+            {
+                int numero = Convert.ToInt32(Interaction.InputBox("Ingrese precios: "));
+                precios[i] = numero;
+                acumulado = acumulado + numero;
+
+            }
+            MessageBox.Show("El numero total de los precios sumados es: " + acumulado);
+        }
+    }
+}
--- Arreglos_IVANKOLARIK/Program.cs
+++ Arreglos_IVANKOLARIK/Program.cs
@@ -16,7 +16,7 @@ namespace Arreglos_IVANKOLARIK
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
